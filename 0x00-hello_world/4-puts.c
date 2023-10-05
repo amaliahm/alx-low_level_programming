0414633a013163e72295f27f77c98470ba4a2ebd
amalia <127314669+amaliahm@users.noreply.github.com>
2023-10-05 17:20:46 -0400

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: C program to print using puts function not printf
+ * Description: C program that prints using puts function
  *
  * Return : Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
