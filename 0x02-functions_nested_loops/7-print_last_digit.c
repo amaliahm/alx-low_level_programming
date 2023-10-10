698566a49b22f004ea5412dfeb7837b3aa7a5c68
amalia <a.ghandouz@esi-sba.dz>
2023-10-10 12:57:20 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,8 +15,9 @@ int print_last_digit(int n)
 	int last;
 
 	if (n < 0)
-		n = (-1) * n;
-	last = n % 10;
+		last = (-1) * (n % 10);
+	else 
+		last = n % 10;
 	_putchar(last + '0');
 	return (last);
 }
