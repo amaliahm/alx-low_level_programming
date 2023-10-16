ec41564c7e7da3e3e7623cd99fe1590cc84633f5
amalia <a.ghandouz@esi-sba.dz>
2023-10-16 10:32:04 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string with new line
+ *
+ * @s: pointer to string input
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
