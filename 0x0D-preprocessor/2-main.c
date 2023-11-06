a7e43d19171d7b7fe49a2de7779f65f09c847d21
amalia <a.ghandouz@esi-sba.dz>
2023-11-05 22:21:15 -0800

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entyr point
+ *
+ * Description: print name of this source file
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
