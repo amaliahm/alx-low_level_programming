c0ecc63d3bb01492bb245558721673492e17dd5b
amalia <127314669+amaliahm@users.noreply.github.com>
2023-11-13 07:13:23 -0500

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	str = malloc(len1 + len2 + 1);
+	str = malloc(len1 + n + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
