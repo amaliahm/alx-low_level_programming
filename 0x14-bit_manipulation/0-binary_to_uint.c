07d65ff2c17a7142882de46ff19b0a86cafe991f
amalia <a.ghandouz@esi-sba.dz>
2023-11-23 07:17:15 -0800

    add binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to string
+ *
+ * @b: binary number input
+ *
+ * Return: int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		i = i * 2 + (*b++ - '0');
+	}
+	return (i);
+}
