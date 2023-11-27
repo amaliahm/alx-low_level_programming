d4430f66a6a31990349771107873c58f73b27fd6
amalia <a.ghandouz@esi-sba.dz>
2023-11-27 06:11:53 -0800

    add append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ *
+ * @filename: string input
+ * @text_content: string input
+ *
+ * Return: int
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i;
+	ssize_t bytes = 0, l = _strlen(text_context);
+
+	if (!filename)
+		return (-1);
+	i = open(filename, O_WRONLY | O_APPEND);
+	if (i == -1)
+		return (-1);
+	if (l)
+		bytes = write(i, text_content, l);
+	close(i);
+	return (bytes == l ? 1 : -1);
+}
